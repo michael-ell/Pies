0c5f424abfb13dbf055a61bae87548c086bdf166
Miguel <codell.concepts@gmail.com>
2013-02-26 21:21:01 -0700

    Added testing library

--- Testing/Properties/AssemblyInfo.cs
+++ Testing/Properties/AssemblyInfo.cs
@@ -3,5 +3,6 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Codell.Pies.Tests")]
-[assembly: AssemblyDescription("Codell Pies - Tests")]
+[assembly: AssemblyTitle("Codell.Pies.Testing")]
+[assembly: AssemblyDescription("Codell Pies - Testing")]
+
