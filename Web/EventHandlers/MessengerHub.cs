9d3e20bbc8f1cb8aef66d32fab486d34ca1f54f8
michael-ell <codell.concepts@gmail.com>
2013-03-04 22:00:12 -0700

    added hub

---
+++ Web/EventHandlers/MessengerHub.cs
@@ -0,0 +1,25 @@
+﻿using Codell.Pies.Common;
+using Codell.Pies.Core.Events;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Ncqrs.Eventing.ServiceModel.Bus;
+
+namespace Codell.Pies.Web.EventHandlers
+{
+    [HubName("messenger")]
+    public class MessengerHub : Hub, IEventHandler<PieStartedEvent>
+    {
+        private readonly IHubContext _hubContext;
+
+        public MessengerHub(IHubContext hubContext)
+        {
+            Verify.NotNull(hubContext, "hubContext");
+            _hubContext = hubContext;
+        }
+
+        public void Handle(IPublishedEvent<PieStartedEvent> evnt)
+        {
+            _hubContext.Clients.All.pieStarted(evnt.EventSourceId);
+        }
+    }
+}
