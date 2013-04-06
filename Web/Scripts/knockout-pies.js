371076367e84457eb3193ff86698493ce1d54237
michael-ell <codell.concepts@gmail.com>
2013-04-06 10:04:23 -0600

    Colored ingredient description to match pie slices

--- Web/Scripts/knockout-pies.js
+++ Web/Scripts/knockout-pies.js
@@ -38,39 +38,27 @@ ko.bindingHandlers.slider = {
 };
 
 ko.bindingHandlers.sliderColor = {
-    update: function (el, valueAccessor) {
+    update: function(el, valueAccessor) {
         var color = ko.utils.unwrapObservable(valueAccessor());
         if (color) {
-            $('.ui-slider-handle', $(el)).css('background', color).css('border-color', ko.bindingHandlers.sliderColor.adjust(color, -50));
+            $('.ui-slider-handle', $(el)).css('background', color).css('border-color', ko.utils.adjustColor(color, -50));
         }
-    },
-    adjust: function(color, amt){
-        var usePound = false;
-        if (color[0] == "#") {
-            color = color.slice(1);
-            usePound = true;
+    }
+};
+
+ko.bindingHandlers.color = {
+    update: function (el, valueAccessor, allBindingsAccessor) {
+        var color = ko.utils.unwrapObservable(valueAccessor());
+        if (color) {
+            var opts = allBindingsAccessor().colorOptions || {backgroundAdjust: 0, borderAdjust: 0};
+            $(el).css('background', ko.utils.adjustColor(color, opts.backgroundAdjust)).css('border-color', ko.utils.adjustColor(color, opts.borderAdjust));
         }
-        
-        var num = parseInt(color, 16);
-        var r = (num >> 16) + amt;
-        if (r > 255) r = 255;
-        else if (r < 0) r = 0;
-        var b = ((num >> 8) & 0x00FF) + amt;
-
-        if (b > 255) b = 255;
-        else if (b < 0) b = 0;
-
-        var g = (num & 0x0000FF) + amt;
-        if (g > 255) g = 255;
-        else if (g < 0) g = 0;
-
-        return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
     }
 };
 
 ko.bindingHandlers.pieChart = {
     instance: null,
-    colors: ['#4572A7', '#AA4643', '#89A54E', '#80699B', '#3D96AE', '#DB843D', '#92A8CD', '#A47D7C', '#B5CA92'],
+    colors: ['#AA4643', '#4572A7', '#89A54E', '#DB843D', '#80699B', '#3D96AE', '#92A8CD', '#A47D7C', '#B5CA92'],
     init: function (el, valueAccessor) {
         ko.bindingHandlers.pieChart.instance = new Highcharts.Chart({
             chart: {
@@ -82,10 +70,6 @@ ko.bindingHandlers.pieChart = {
             title: {
                 text: ''
             },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage}%</b>',
-                percentageDecimals: 1
-            },
             plotOptions: {
                 pie: {
                     allowPointSelect: true,
@@ -95,7 +79,7 @@ ko.bindingHandlers.pieChart = {
                         color: '#000000',
                         connectorColor: '#000000',
                         formatter: function () {
-                            return '<b>' + this.point.name + '</b>: ' + this.percentage + ' %';
+                            return '<b>' + this.point.name + '</b>';
                         }
                     }
                 }
@@ -103,7 +87,6 @@ ko.bindingHandlers.pieChart = {
             series: [{
                 type: 'pie',
                 data: ko.utils.unwrapObservable(valueAccessor()),
-                name: 'My Pie'
             }]
         });
     },
@@ -124,3 +107,26 @@ ko.bindingHandlers.pieChart = {
         setTimeout(function () { ko.bindingHandlers.pieChart.instance.redraw(); }, 1000);
     }
 };
+
+ko.utils.adjustColor = function(color, amt) {
+    var usePound = false;
+    if (color[0] == "#") {
+        color = color.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(color, 16);
+    var r = (num >> 16) + amt;
+    if (r > 255) r = 255;
+    else if (r < 0) r = 0;
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if (b > 255) b = 255;
+    else if (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+
+    return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+}
