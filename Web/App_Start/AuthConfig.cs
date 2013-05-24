b6f3cbf2e0722ae8d08e74feb67bbbf41c88c8ec
michael-ell <codell.concepts@gmail.com>
2013-05-23 21:36:49 -0600

    Added ability to authenticate against MS and Twitter

--- Web/App_Start/AuthConfig.cs
+++ Web/App_Start/AuthConfig.cs
@@ -6,27 +6,15 @@ namespace Codell.Pies.Web.App_Start
     {
         public static void RegisterAuth()
         {
-            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
-            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
-
-            //OAuthWebSecurity.RegisterMicrosoftClient(
-            //    clientId: "000000004C0E9729",
-            //    clientSecret: "SdfclQdBhKYKkFUX6-qrkZM-guaU-FDj");
-
-            //OAuthWebSecurity.RegisterTwitterClient(
-            //    consumerKey: "",
-            //    consumerSecret: "");
+            OAuthWebSecurity.RegisterGoogleClient();
+            OAuthWebSecurity.RegisterMicrosoftClient(clientId: "000000004C0E9729", clientSecret: "SdfclQdBhKYKkFUX6-qrkZM-guaU-FDj");
+            OAuthWebSecurity.RegisterTwitterClient(consumerKey: "wMqGbIeeaXxYCHHcXa7CzA", consumerSecret: "4lttnxAhJznWujuAfQSz7MVHun6Eq3hd4gJeiYZ9KU4");
 
             //OAuthWebSecurity.RegisterFacebookClient(
             //    appId: "",
             //    appSecret: "");
 
-            //OAuthWebSecurity.RegisterLinkedInClient(
-            //    appId: "",
-            //    appSecret: "");
-
-
-            OAuthWebSecurity.RegisterGoogleClient();
+            //OAuthWebSecurity.RegisterLinkedInClient(consumerKey: "xommx7yio9ei", consumerSecret: "d9ATsd2vitInuWvB");
         }
     }
 }
