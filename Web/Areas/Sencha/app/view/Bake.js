9ebe3ed2b4c52df8eaac0a74828c5cb842ab1fc3
michael-ell <codell.concepts@gmail.com>
2013-11-11 21:10:33 -0700

    test commit

--- Web/Areas/Sencha/app/view/Bake.js
+++ Web/Areas/Sencha/app/view/Bake.js
@@ -5,21 +5,12 @@ Ext.define('Pies.view.Bake', {
     config: {
         title: 'Bake',
         iconCls: 'action',
-        //layout: 'hbox',
         items: [
             {
                 docked: 'top',
                 xtype: 'titlebar',
                 title: 'Bake'
-            },           
-            //{
-            //    html: 'Red',
-            //    style: 'background-color: #B22222; color: white;',
-            //},
-            //{
-            //    html: 'Second Panel',
-            //    style: 'background-color: #5E99CC;',
-            //}
+            }
         ]
     }
 });
