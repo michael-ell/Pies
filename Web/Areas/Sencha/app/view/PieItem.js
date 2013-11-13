af00909eb7152793da092bd056030cd491912230
michael-ell <codell.concepts@gmail.com>
2013-11-12 21:41:27 -0700

    dynamically created pie store using ingredients and colors from recent pies

--- Web/Areas/Sencha/app/view/PieItem.js
+++ Web/Areas/Sencha/app/view/PieItem.js
@@ -3,49 +3,25 @@ Ext.define('Pies.view.PieItem', {
     requires: [
         'Ext.chart.PolarChart',
         'Ext.chart.series.Pie',
-        'Ext.Button',
         'Pies.model.Ingredient'
     ],
     xtype: 'pieItem',
     config: {
-        pie: true,
-        //nameButton: true,
-        dataMap: {
-            //getPie: {                
-                //setStore: Ext.create('Ext.data.Store', { model: 'Pies.model.Ingredient', data: 'allIngredients' }),
-            //},
-            //getNameButton: {
-            //    setText: 'caption'
-            //}
-        }        
+        pie: {}       
     },
     applyPie: function () {
-     
-        //return Ext.factory({
-        //    colors: ["#115fa6", "#94ae0a", "#a61120", "#ff8809", "#ffd13e"],
-        //    //store: Ext.create('Ext.data.Store', { model: 'Pies.model.Ingredient', data: r[0].data.allIngredients }),
-        //    store: {
-        //        fields: ['description', 'percent'],
-        //        data: [{ description: 'a', percent: 20 }, { description: 'b', percent: 30 }, { description: 'c', percent: 50 }]
-        //    },
-        //    series: [{
-        //        type: 'pie',
-        //        labelField: 'description',
-        //        xField: 'percent'
-        //    }]
-        //}, Ext.chart.PolarChart, this.getPie());
-        //var container = Ext.create('Ext.Panel', {
-            
-        //});
+        window.console.log(this.config.record.data.caption);
+
+        var ingredients = this.config.record.data.allIngredients;
+        var colors = [];
+        for (var i = 0; i < ingredients.length; i++) {
+            colors.push(ingredients[i].color);
+        }
         
         return Ext.create('Ext.chart.PolarChart', {
             height: 200, 
-            colors: ["#115fa6", "#94ae0a", "#a61120", "#ff8809", "#ffd13e"],
-            //store: Ext.create('Ext.data.Store', { model: 'Pies.model.Ingredient', data: r[0].data.allIngredients }),
-            store: {
-                fields: ['description', 'percent'],
-                data: [{ description: 'axx', percent: 20 }, { description: 'b', percent: 30 }, { description: 'c', percent: 50 }]
-            },
+            colors: colors,
+            store: Ext.create('Ext.data.Store', { model: 'Pies.model.Ingredient', data: ingredients }),
             series: [{
                 type: 'pie',
                 labelField: 'description',
@@ -61,16 +37,4 @@ Ext.define('Pies.view.PieItem', {
             this.add(newPie);
         }
     },
-
-    //applyNameButton: function (config) {
-    //    return Ext.factory(config, Ext.Button, this.getNameButton());
-    //},
-    //updateNameButton: function (newButton, oldButton) {
-    //    if (newButton) {
-    //        this.add(newButton);
-    //    }
-    //    if (oldButton) {
-    //        this.remove(oldButton);
-    //    }
-    //}
 });
