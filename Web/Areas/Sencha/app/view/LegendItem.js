4107d608b5c552c4a6b5e96c7b7391b1db001761
michael-ell <codell.concepts@gmail.com>
2013-11-16 23:08:02 -0700

    added missed files

---
+++ Web/Areas/Sencha/app/view/LegendItem.js
@@ -0,0 +1,23 @@
+Ext.define('Pies.view.LegendItem', {
+    extend: 'Ext.dataview.component.DataItem',
+    requires: [],
+    xtype: 'legendItem',
+    config: {
+        legend: {}       
+    },
+    applyLegend: function () {
+        var item = this.config.record.data;
+        var marker = "<span class='legend-marker' style='background-color:" + item.mark + "'></span>";
+        return Ext.create('Ext.Container', {
+            html: "<div class='legend-item-container'><div class='legend-item'>" + marker + item.name + "</div></div>"
+        });
+    },
+    updateLegend: function (newLegend, oldLegend) {
+        if (oldLegend) {
+            this.remove(oldLegend);
+        }
+        if (newLegend) {
+            this.add(newLegend);
+        }
+    },
+});
