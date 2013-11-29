dec6f9879577d786c72a85938da6e8fb969df173
michael-ell <codell.concepts@gmail.com>
2013-11-28 21:11:01 -0700

    Moved creation of pie back into controller

--- Web/Areas/Sencha/app/view/Home.js
+++ Web/Areas/Sencha/app/view/Home.js
@@ -23,63 +23,14 @@ Ext.define('Pies.view.Home', {
             {
                 xtype: 'carousel',
                 id: 'pies',
-                store: Ext.create('Pies.store.RecentPies'),
-                listeners: {
-                    initialize: function (carousel) {
-                        Ext.getStore('recentPies').load(function (recent) {
-                            Ext.each(recent, function (pie) {
-                                var ingredients = pie.data.allIngredients;
-                                var colors = [];
-                                for (var i = 0; i < ingredients.length; i++) {
-                                    colors.push(ingredients[i].color);
-                                }
-
-                                var chart = Ext.create('Ext.chart.PolarChart', {
-                                    height: 200,
-                                    colors: colors,
-                                    store: Ext.create('Ext.data.Store', { model: 'Pies.model.Ingredient', data: ingredients }),
-                                    series: [{
-                                        type: 'pie',
-                                        labelField: 'description',
-                                        xField: 'percent',
-                                        renderer: function (sprite) {
-                                            sprite.attr.doCallout = false;
-                                            sprite.attr.label = '';
-                                        }
-                                    }]
-                                });
-
-                                var container = Ext.create('Ext.Container', {
-                                    items: [
-                                        {
-                                            xtype: 'container',
-                                            html: '<div class="header">' + pie.data.caption + '</div>'
-                                        },
-                                        chart,
-                                        {
-                                            xtype: 'dataview',
-                                            cls: 'legend',
-                                            scrollable: 'vertical',
-                                            store: chart.getLegendStore(),
-                                            useComponents: true,
-                                            defaultType: 'legendItem',
-                                        }
-                                    ]
-                                });
-                                carousel.add(container);
-                            });
-                        });
-                        //carousel.setItems(items);
-                        carousel.setActiveItem(0);
-                    }
-                }
+                store: Ext.create('Pies.store.RecentPies')
             }
         ],
         //listeners: {
         //    //Controller ref / control listener
         //    activate: function () {
-        //        //Ext.getStore('recentPies').load();
-        //        //this.fireEvent('onLoadPies', this);
+        //        Ext.getStore('recentPies').load();
+        //        //this.fireEvent('onLoadPies');
         //    }
         //}
     }
