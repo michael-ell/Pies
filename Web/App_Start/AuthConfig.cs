e7de2642fff0f9f845c003565069a344392c9588
michael-ell <codell.concepts@gmail.com>
2014-04-10 16:47:02 -0600

    Setup logging. Added logging for authentication setup.

--- Web/App_Start/AuthConfig.cs
+++ Web/App_Start/AuthConfig.cs
@@ -1,16 +1,27 @@
-﻿using Codell.Pies.Web.Security;
+﻿using System;
+using Codell.Pies.Web.Security;
+using Common.Logging;
 using Microsoft.Web.WebPages.OAuth;
 
 namespace Codell.Pies.Web.App_Start
 {
     public static class AuthConfig
     {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+
         public static void RegisterAuth()
         {
-            var configuration = new AuthenticationClientConfiguration();
-            OAuthWebSecurity.RegisterGoogleClient();      
-            OAuthWebSecurity.RegisterMicrosoftClient(configuration.Microsoft.Key, configuration.Microsoft.Secret);
-            OAuthWebSecurity.RegisterTwitterClient(configuration.Twitter.Key, configuration.Twitter.Secret);
+            try
+            {
+                var configuration = new AuthenticationClientConfiguration();
+                OAuthWebSecurity.RegisterGoogleClient();
+                OAuthWebSecurity.RegisterMicrosoftClient(configuration.Microsoft.Key, configuration.Microsoft.Secret);
+                OAuthWebSecurity.RegisterTwitterClient(configuration.Twitter.Key, configuration.Twitter.Secret);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+            }
 
             //OAuthWebSecurity.RegisterFacebookClient(
             //    appId: "",
